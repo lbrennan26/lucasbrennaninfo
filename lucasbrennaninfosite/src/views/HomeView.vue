ed541702d3e90c20bd35c33471d3edac04da5be6
lucas <lub21@pitt.edu>
2023-04-16 19:06:52 -0400

    some minor updates, playing around with icons

--- lucasbrennaninfosite/src/views/HomeView.vue
+++ lucasbrennaninfosite/src/views/HomeView.vue
@@ -1,11 +1,60 @@
-<script setup>
-
+<script>
+  export default {
+    data: () => ({
+      drawer: false,
+      group: null,
+      items: [
+        {
+          title: 'Foo',
+          value: 'foo',
+        },
+        {
+          title: 'Bar',
+          value: 'bar',
+        },
+        {
+          title: 'Fizz',
+          value: 'fizz',
+        },
+        {
+          title: 'Buzz',
+          value: 'buzz',
+        },
+      ],
+    }),
+    watch: {
+      group () {
+        this.drawer = false
+      },
+    },
+  }
 </script>
 
 <template>
   <main>
     <v-app>
-      <v-app-bar :elevation="2"></v-app-bar>
+      <v-app-bar 
+      :elevation="2">
+      <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+<v-toolbar-title>My files</v-toolbar-title>
+
+<v-spacer></v-spacer>
+
+<v-btn variant="text" icon="mdi-magnify"></v-btn>
+
+<v-btn variant="text" icon="mdi-filter"></v-btn>
+
+<v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer
+        v-model="drawer"
+      >
+        <v-list
+          :items="items"
+        ></v-list>
+      </v-navigation-drawer>
     </v-app>
   </main>
 </template>
