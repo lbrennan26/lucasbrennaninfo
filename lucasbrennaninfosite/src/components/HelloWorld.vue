5352854f9784504ae9967f3ce828fad7975f7395
lucas <lub21@pitt.edu>
2023-07-07 22:03:32 -0400

    basic wireframing

--- lucasbrennaninfosite/src/components/HelloWorld.vue
+++ lucasbrennaninfosite/src/components/HelloWorld.vue
@@ -1,71 +1,43 @@
 <template>
   <v-container class="fill-height">
     <v-responsive class="align-center text-center fill-height">
-      <v-img height="300" src="@/assets/logo.svg" />
-
-      <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
-
-      <h1 class="text-h2 font-weight-bold">Vuetify</h1>
-
-      <div class="py-14" />
-
-      <v-row class="d-flex align-center justify-center">
-        <v-col cols="auto">
-          <v-btn
-            href="https://vuetifyjs.com/components/all/"
-            min-width="164"
-            rel="noopener noreferrer"
-            target="_blank"
-            variant="text"
+      <v-row>
+          <v-col
+            cols="12"
+            sm="2"
           >
-            <v-icon
-              icon="mdi-view-dashboard"
-              size="large"
-              start
-            />
+            <v-sheet
+              rounded="lg"
+              min-height="268"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
 
-            Components
-          </v-btn>
-        </v-col>
-
-        <v-col cols="auto">
-          <v-btn
-            color="primary"
-            href="https://vuetifyjs.com/introduction/why-vuetify/#feature-guides"
-            min-width="228"
-            rel="noopener noreferrer"
-            size="x-large"
-            target="_blank"
-            variant="flat"
+          <v-col
+            cols="12"
+            sm="8"
           >
-            <v-icon
-              icon="mdi-speedometer"
-              size="large"
-              start
-            />
+            <v-sheet
+              min-height="70vh"
+              rounded="lg"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
 
-            Get Started
-          </v-btn>
-        </v-col>
-
-        <v-col cols="auto">
-          <v-btn
-            href="https://community.vuetifyjs.com/"
-            min-width="164"
-            rel="noopener noreferrer"
-            target="_blank"
-            variant="text"
+          <v-col
+            cols="12"
+            sm="2"
           >
-            <v-icon
-              icon="mdi-account-group"
-              size="large"
-              start
-            />
-
-            Community
-          </v-btn>
-        </v-col>
-      </v-row>
+            <v-sheet
+              rounded="lg"
+              min-height="268"
+            >
+            
+            </v-sheet>
+          </v-col>
+        </v-row>
     </v-responsive>
   </v-container>
 </template>
--- lucasbrennaninfosite/src/layouts/default/AppBar.vue
+++ lucasbrennaninfosite/src/layouts/default/AppBar.vue
@@ -1,13 +1,26 @@
 <template>
-  <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" />
-
-      Essentials Preset
-    </v-app-bar-title>
+    <v-app-bar
+      class="px-3"
+      color="white"
+      :elevation="2"
+      density="compact"
+    >
+    <v-btn @click.stop="goToHome">
+          Home
+     </v-btn>
+     <v-btn @click.stop="goToAbout">
+          About Me
+    </v-btn>
   </v-app-bar>
 </template>
 
 <script setup>
-  //
+import { useRouter } from 'vue-router';
+const router = useRouter()
+function goToAbout() {
+  router.push('/about')
+}
+function goToHome() {
+  router.push('/')
+}
 </script>
