9c7a7c83d2ea82a24609ff4032bdf1c44816444b
lucas <lub21@pitt.edu>
2023-07-07 21:50:07 -0400

    major scaffolding changes

--- lucasbrennaninfosite/src/main.js
+++ lucasbrennaninfosite/src/main.js
@@ -1,38 +1,20 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+/**
+ * main.js
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
 
-
+// Components
 import App from './App.vue'
-import router from './router'
-import './assets/main.css'
-//icons
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-/* import specific icons */
-import { faBars } from '@fortawesome/free-solid-svg-icons'
-library.add(faBars)
+// Composables
+import { createApp } from 'vue'
 
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-
-const vuetify = createVuetify({
-  components,
-  directives,
-  theme: {
-    defaultTheme: 'dark'
-  }
-})
-
+// Plugins
+import { registerPlugins } from '@/plugins'
 
 const app = createApp(App)
-.component('font-awesome-icon', FontAwesomeIcon)
-app.use(createPinia())
-app.use(router)
-app.use(vuetify)
+
+registerPlugins(app)
 
 app.mount('#app')
